8e5166cf6778c6bfe2ac7f898e9cf6561bf03c95
boltoncouncil <73642724+boltoncouncil@users.noreply.github.com>
2020-11-02 19:27:56 +0000

    Update custom2.js

--- resources/js/custom2.js
+++ resources/js/custom2.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+
 	
 function formName(){
     
@@ -86,15 +86,7 @@ function regexSearch(regex, selector){
 
 }
 
-// add a non selectable option on search results to indicate if results found or not
-	 $(formName()).on('_KDF_search', function(event, kdf, response, type, name) {
-	     console.log('first')
-		//KS: call noResultsFound with 'this' set to the search element that triggered the event
-		noResultsFound.call($('[name="'+name+'_id"]'));
-		//KS: call noResultsFound with 'this' set to the search element that triggered the event
-		selectResult.call($('[name="'+name+'_id"]'));
-		
-	 });
+
 
 function applyStyles(){
 
@@ -139,7 +131,15 @@ function applyStyles(){
 	                               .addClass('btn btn-secondary')
 	                               .text('Search again');
 	 
-	
+	// add a non selectable option on search results to indicate if results found or not
+	 $(formName()).on('_KDF_search', function(event, kdf, response, type, name) {
+	     console.log('first')
+		//KS: call noResultsFound with 'this' set to the search element that triggered the event
+		noResultsFound.call($('[name="'+name+'_id"]'));
+		//KS: call noResultsFound with 'this' set to the search element that triggered the event
+		selectResult.call($('[name="'+name+'_id"]'));
+		
+	 });
 	
 
 	 // hide success message on complete
@@ -150,4 +150,4 @@ function applyStyles(){
 	 });
 }
 
-});	
+
